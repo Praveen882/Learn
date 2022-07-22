57fe98388cc60769382e18c1ecdee4f1061c06b6
PRAVEEN N <88902362+Praveen882@users.noreply.github.com>
2022-07-22 22:45:17 +0530

    list_dictionary_tuple

---
+++ list_dic_tuple.py
@@ -0,0 +1,20 @@
+
+list1 = [12,1,"world"]
+list1[1] = "wolf"
+print (list1)
+
+tuple1 = ("world",123,"you")
+
+print (tuple1)
+
+set = set([12,34,56])
+
+print(set)
+
+dict1 = {
+    1: "scm",
+    2: "psg",
+    3: "mcet"
+}
+dict1[12] = "adding"
+print(dict1)
